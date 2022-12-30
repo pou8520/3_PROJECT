ab15910188785e2ad2f6149369a5fbafefe08560
ByunJungMin <cvy5375@naver.com>
2022-12-30 23:16:58 +0900

    owner_order css

--- assets/static/JS/owner.js
+++ assets/static/JS/owner.js
@@ -1,3 +1,10 @@
-$(document).ready(function () {
+function modal_open() { 
+    $(".modal").fadeIn();
 
-});
+    $(document).mouseup(function (e) {
+        if($(".modal").has(e.target).length === 0) {
+            $(".modal").hide();
+        }
+    })
+    
+};
