329ffe04379e21a8949fa9a10c14fadd48a59ce2
ByunJungMin <cvy5375@naver.com>
2022-12-30 19:24:44 +0900

    폴더 나누기

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-
-
+const router = require('./routes')
 require('dotenv').config();
+
+app.use('/api',express.json(), router)
 
 app.listen(process.env.PORT, () => {
     console.log(`${process.env.PORT} 포트가 열렸어요 `);
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const ownerRouter = require('./owner_routes');
+
+router.use('/orders', ownerRouter);
+
+module.exports = router;
