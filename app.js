c4a4e8a282c122abc82b0f8a9813cda5056f78af
vividnow <belucent0@gmail.com>
2023-01-02 10:26:03 +0900

    default

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+require('dotenv').config();
+
+
+const orderRouter = require("./routes/orders")
 const app = express();
 
 
-require('dotenv').config();
+app.use(express.json());  //JSON 형태의 데이터 처리하기 위한 설정
+app.use('/orders', ordersRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`${process.env.PORT} 포트가 열렸어요 `);
